38e2bc1643c8edaecac5925ce3d25f48445d8ab2
Itsuki Urakawa <urakawa.itsuki.78z@st.kyoto-u.ac.jp>
2024-09-04 11:45:33 +0900

    Implement omp2

--- openmp/omp2.c
+++ openmp/omp2.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<omp.h>
 
-#define N 10000000000LL
+#define N 1000000000000LL
 
 int main(){
 
@@ -11,6 +11,7 @@ int main(){
 
   t1 = omp_get_wtime();
 
+  #pragma omp parallel for reduction(+: sum)
   for(k = N-1; k > 0; k -= 2){
     sum -= 1.0 / (2*k+1);
     sum += 1.0 / (2*k-1);
