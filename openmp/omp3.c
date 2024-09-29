9fb2b06ee9afa8b12eada9aba6052ca94c1d5201
Itsuki Urakawa <urakawa.itsuki.78z@st.kyoto-u.ac.jp>
2024-09-29 12:32:45 +0900

    Implement omp3_2

--- openmp/omp3.c
+++ openmp/omp3.c
@@ -14,18 +14,33 @@ int main(){
   min = 97;
   min_idx = 0;
 
-  for(i = L; i <= R; i++){
-    t = i;
-    for(j = 2; j*j <= i; j++){
-      if(i%j == 0){
-        t = j;
-        break;
+  int chunk_size = 1000;
+  #pragma omp parallel
+  {
+    long long local_min = min;
+    long long local_min_idx = min_idx;
+    #pragma omp for private(j) schedule(dynamic, chunk_size)
+    for(i = L; i <= R; i++){
+      t = i;
+      for(j = 2; j*j <= i; j++){
+        if(i%j == 0){
+          t = j;
+          break;
+        }
+      }
+    
+      if (local_min > t){
+        local_min = t;
+        local_min_idx = i;
       }
     }
-    
-    if (min > t){
-      min = t;
-      min_idx = i;
+
+    #pragma omp critical
+    {
+      if (local_min < min){
+        min = local_min;
+        min_idx = local_min_idx;
+      }
     }
   }
 
